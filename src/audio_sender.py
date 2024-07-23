8087e04a7ce762a3ad507362d69cbba79e21ff49
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-07-23 21:27:58 +0200

    Added output audio-Txt to the transcriptions.log

--- src/audio_sender.py
+++ src/audio_sender.py
@@ -1,30 +1,32 @@
 # src/audio_sender.py
+import logging
 from dotenv import load_dotenv
 import os
-
 import requests
 
 class AudioSender:
     def __init__(self, server_url):
         self.server_url = server_url
+        logging.basicConfig(filename='transcriptions.log', level=logging.INFO, format='%(asctime)s - %(message)s', encoding='utf-8')
+        self.logger = logging.getLogger()
 
     def send_audio(self, file_path, segment_number):
-        # Open the audio file and prepare the POST request
         with open(file_path, 'rb') as file:
             files = {'file': file}
             data = {'segment': segment_number}
             response = requests.post(self.server_url, files=files, data=data)
-
-        print(f"Status Code: {response.status_code}")
-
         try:
             response_json = response.json()
             if response.status_code == 200:
-                print(f"Transcription: {response_json.get('transcription', 'No transcription found')}")
+                transcription = response_json.get('transcription', 'No transcription found')
+                self.logger.info(f"Transcription: {transcription}")
             else:
-                print(f"Error: {response_json.get('error', 'Unknown error')}")
-                print(f"Reason: {response_json.get('reason', 'No reason provided')}")
+                error_message = response_json.get('error', 'Unknown error')
+                reason = response_json.get('reason', 'No reason provided')
+                self.logger.info(f"Status Code: {response.status_code}")
+                self.logger.error(f"Error: {error_message}")
+                self.logger.error(f"Reason: {reason}")
         except ValueError:
-            print("Failed to parse response as JSON")
+            self.logger.error("Failed to parse response as JSON")
 
         return response.status_code
