1dee1610426c9997a627b40439891862f9185a02
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-07-26 10:15:33 +0200

    transfer sending data to async mode

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 from src.audio_recorder import AudioRecorder
 from src.audio_sender import AudioSender
 import time
+import asyncio
 from dotenv import load_dotenv
 import os
 
-def main():
+load_dotenv()
+
+async def main():
     server_url = os.getenv('SERVER_URL')
     segment_number = 0
 
@@ -14,17 +17,20 @@ def main():
     sender = AudioSender(server_url)
     segment_pause_sec = round(float(os.getenv('SEGMENT_PAUSE_SEC')), 1)
 
+    tasks = []
+
     try:
         while True:
             audio_file = recorder.record_segment()
             if audio_file and os.path.getsize(audio_file) > 0:
                 segment_number += 1
-                status = sender.send_audio(audio_file, segment_number)
-                print(f"Segment {segment_number} sent, status: {status}")
+                task = asyncio.create_task(sender.send_audio(audio_file, segment_number))
+                tasks.append(task)
 
-            time.sleep(segment_pause_sec)  # Wait for a X second before recording the next segment
+            await asyncio.sleep(segment_pause_sec)
+
     except KeyboardInterrupt:
         print("Recording stopped")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- src/audio_sender.py
+++ src/audio_sender.py
@@ -2,10 +2,14 @@
 import logging
 from dotenv import load_dotenv
 import os
+import aiohttp
+import asyncio
+import ssl
 import urllib3
-import requests
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+load_dotenv()
 
 class AudioSender:
     def __init__(self, server_url):
@@ -13,25 +17,40 @@ class AudioSender:
         logging.basicConfig(filename='transcriptions.log', level=logging.INFO, format='%(asctime)s - %(message)s', encoding='utf-8')
         self.logger = logging.getLogger()
 
-    def send_audio(self, file_path, segment_number):
-        with open(file_path, 'rb') as file:
-            files = {'file': file}
-            data = {'segment': segment_number}
-            AUTHORIZATION = os.getenv('AUTHORIZATION')
-            prod_mode = int(os.getenv('PROD_MODE'))
-            response = requests.post(self.server_url, files=files, data=data, headers={'Authorization': AUTHORIZATION}, verify=prod_mode)
-        try:
-            response_json = response.json()
-            if response.status_code == 200:
-                transcription = response_json.get('transcription', 'No transcription found')
-                self.logger.info(f": {transcription}")
-            else:
-                error_message = response_json.get('error', 'Unknown error')
-                reason = response_json.get('reason', 'No reason provided')
-                self.logger.info(f"Status Code: {response.status_code}")
-                self.logger.error(f"Error: {error_message}")
-                self.logger.error(f"Reason: {reason}")
-        except ValueError:
-            self.logger.error("Failed to parse response as JSON")
+    async def send_audio(self, file_path, segment_number):
+        AUTHORIZATION = os.getenv('AUTHORIZATION')
+        prod_mode = int(os.getenv('PROD_MODE'))
+        
+        # Create an SSL context to disable SSL verification if prod_mode is 0
+        if prod_mode == 0:
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+        else:
+            ssl_context = ssl.create_default_context()
 
-        return response.status_code
+        connector = aiohttp.TCPConnector(ssl=ssl_context)
+
+        async with aiohttp.ClientSession(connector=connector) as session:
+            with open(file_path, 'rb') as file:
+                data = aiohttp.FormData()
+                data.add_field('file', file, filename=os.path.basename(file_path))
+                data.add_field('segment', str(segment_number))
+                async with session.post(self.server_url, data=data, headers={'Authorization': AUTHORIZATION}) as response:
+                    self.logger.info(f"Status Code: {response.status}")
+
+                    try:
+                        print(f"Segment {segment_number} sent asynchronously")
+                        response_json = await response.json()
+                        if response.status == 200:
+                            transcription = response_json.get('transcription', 'No transcription found')
+                            self.logger.info(f"Transcription: {transcription}")
+                        else:
+                            error_message = response_json.get('error', 'Unknown error')
+                            reason = response_json.get('reason', 'No reason provided')
+                            self.logger.error(f"Error: {error_message}")
+                            self.logger.error(f"Reason: {reason}")
+                    except aiohttp.ContentTypeError:
+                        self.logger.error("Failed to parse response as JSON")
+
+        return response.status
